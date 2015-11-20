6a709edb03f7952fa6ad0bf60a64543e5d8d884d
David Micciche <gurafity@gmail.com>
2015-11-20 23:07:33 +0100

    Project Creation
    project creation + first class Pawn

---
+++ Othello/src/be/ephec/othelloServer/Pawn.java
@@ -0,0 +1,13 @@
+package be.ephec.othelloServer;
+
+/**
+ * @author David Micciche
+ * @version 0.0
+ *
+ */
+public class Pawn {
+ //there will be the object Pawn, with 3 potential states, WHITE, BLACK or NONE;
+	public Pawn() {
+		//TO DO
+	}
+}
