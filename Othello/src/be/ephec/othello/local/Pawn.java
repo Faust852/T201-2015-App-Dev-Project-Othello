0f7c51652b245d3e111f3727c8b38ffd6f7d99d8
David Micciche <gurafity@gmail.com>
2015-12-16 19:08:21 +0100

    commitend

---
+++ Othello/src/be/ephec/othello/local/Pawn.java
@@ -0,0 +1,54 @@
+package be.ephec.othello.local;
+
+/**
+ * @author David Micciche
+ * @version 0.0
+ *
+ */
+public class Pawn {
+	protected int stateOfPawn;
+	
+	private static final int blackPawn =  -1;
+	private static final int whitePawn = 1;
+	private static final int nonePawn = 0;
+	private static final int possiblePawn = 2;
+	
+	public Pawn(){
+	}
+	public Pawn(int stateOfPawn) {
+			this.stateOfPawn=stateOfPawn;
+	}
+
+	public int getValueOfPawn() {
+		return this.stateOfPawn;
+	}
+	
+	public void setValueOfPawn(int stateOfPawn) {
+		this.stateOfPawn = stateOfPawn;
+	}
+
+	public static int getBlackPawn() {
+		return blackPawn;
+	}
+
+	public static int getWhitePawn() {
+		return whitePawn;
+	}
+
+	public static int getNonePawn() {
+		return nonePawn;
+	}
+
+	public static int getPossiblePawn() {
+		return possiblePawn;
+	}
+
+	public Pawn getOppositeColorPawn() {
+		Pawn oppositeColorPawn = new Pawn(0);
+		if(this.getValueOfPawn() == 1) {oppositeColorPawn.setValueOfPawn(Pawn.getBlackPawn());}
+		else if(this.getValueOfPawn() == -1) {oppositeColorPawn.setValueOfPawn(Pawn.getWhitePawn());}
+		else if(this.getValueOfPawn() == 0) {oppositeColorPawn.setValueOfPawn(Pawn.getNonePawn());}
+		return oppositeColorPawn;
+	}
+}
+
--- Othello/src/be/ephec/othelloServer/Param.java
+++
@@ -1,6 +0,0 @@
-package be.ephec.othelloServer;
-
-public class Param {
-	
-	public static final int NUMPORTDEBASE = 62900;
-}
